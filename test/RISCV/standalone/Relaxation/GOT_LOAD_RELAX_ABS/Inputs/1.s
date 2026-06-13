f1d3d9bd9ebe0db62c01547534e7e3e1526498f8
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-06-13 14:17:02 -0700

    WIP, consolidating tests
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/1.s
+++ test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/1.s
@@ -1,9 +1,15 @@
 .option relax
 .option pic
 
+.weak sym_wu
+
 .text
 .global _start
 _start:
     lga a0, sym
     lga a1, sym_neg
     lga a2, sym_addi
+
+.global foo
+foo:
+    lga a0, sym_wu
--- test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/weak_undef.s
+++
@@ -1,9 +0,0 @@
-.option relax
-.option pic
-
-.weak sym_wu
-
-.text
-.global foo
-foo:
-    lga a0, sym_wu
