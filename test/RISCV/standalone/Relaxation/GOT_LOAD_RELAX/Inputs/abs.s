0eb786b556a5e5ca86a4eb4b73f6440fd7b8205c
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-06-14 16:48:30 -0700

    WIP, minor reg rework
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX/Inputs/abs.s
+++ test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX/Inputs/abs.s
@@ -7,11 +7,11 @@ foo:
     lga a0, sym_cli_pos
     lga a1, sym_cli_neg
     lga a2, sym_cli_pos_oob
-    lga a4, sym_cli_neg_oob
+    lga a3, sym_cli_neg_oob
     lga a0, sym_addi_pos
     lga a1, sym_addi_neg
     lga a2, sym_addi_pos_oob
     lga a3, sym_addi_neg_oob
-    lga a3, sym_zero
+    lga a0, sym_zero
     lga zero, sym_cli_pos
-    lga a0, sym_hidden
+    lga a1, sym_hidden
