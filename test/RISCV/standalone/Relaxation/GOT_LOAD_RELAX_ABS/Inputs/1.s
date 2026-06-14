a3e518a514355dd12760a566f60bc4298e3bf0cb
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-06-13 17:21:39 -0700

    WIP, unify naming
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/1.s
+++ test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/1.s
@@ -8,8 +8,8 @@
 _start:
     lga a0, sym_cli_pos
     lga a1, sym_cli_neg
-    lga a2, sym_addi_pos
-    lga a4, sym_addi_neg
+    lga a2, sym_cli_pos_oob
+    lga a4, sym_cli_neg_oob
     lga a3, sym_zero
 
 .global foo
@@ -18,7 +18,7 @@ foo:
 
 .global bar
 bar:
-    lga a0, sym_addi_pos_hi
-    lga a1, sym_addi_neg_lo
+    lga a0, sym_addi_pos
+    lga a1, sym_addi_neg
     lga a2, sym_addi_pos_oob
     lga a3, sym_addi_neg_oob
