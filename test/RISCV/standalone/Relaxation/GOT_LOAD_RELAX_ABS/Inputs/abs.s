5f083a3c0f6d4f0d5c488d1dbf8fc10455f45b25
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-06-13 20:49:18 -0700

    WIP, reorder zero tests
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/abs.s
+++ test/RISCV/standalone/Relaxation/GOT_LOAD_RELAX_ABS/Inputs/abs.s
@@ -8,11 +8,8 @@ _start:
     lga a1, sym_cli_neg
     lga a2, sym_cli_pos_oob
     lga a4, sym_cli_neg_oob
-    lga a3, sym_zero
-
-.global bar
-bar:
     lga a0, sym_addi_pos
     lga a1, sym_addi_neg
     lga a2, sym_addi_pos_oob
     lga a3, sym_addi_neg_oob
+    lga a3, sym_zero
